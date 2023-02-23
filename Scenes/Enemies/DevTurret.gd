7b6fdbdaa7197840c0049582fe1fd22c0d7d1e65
MenacingMachina <menacingmachina@gmail.com>
2023-02-23 09:59:46 -0500

    Added nav mesh and nav agent to enemy

--- Scenes/Enemies/DevTurret.gd
+++ Scenes/Enemies/DevTurret.gd
@@ -10,6 +10,8 @@ var hf_pos: Vector3
 var run_speed = 3
 var accel = 3
 var player = null
+@onready var nav_agent:NavigationAgent3D = $NavigationAgent3D
+@onready var repath_timer:Timer = $RepathTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,27 +26,44 @@ func _ready():
 
 	gun.position = hf_pos
 	head.add_child(gun)
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	nav_agent.path_desired_distance = 0.5
+	nav_agent.target_desired_distance = 0.5
 
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(self.global_transform.origin)
+
+func set_movement_target(movement_target : Vector3):
+	nav_agent.set_target_position(movement_target)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _physics_process(delta):
-	print(str(velocity) + " Start Velocity")
-	
-	velocity.y -= gravity * delta
+	velocity.y -= gravity * delta	
+	if nav_agent.is_navigation_finished():
+		return
 	if player:
-		look_at(player.global_transform.origin, Vector3.UP)		
-		var direction = (player.global_transform.origin - global_transform.origin).normalized()
-		velocity.x = direction.x * run_speed
-		velocity.z = direction.z * run_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, accel)
-		velocity.z = move_toward(velocity.z, 0, accel)
-	print(str(velocity) + " End Velocity")
-	print(str(self.get_global_position()) + " Position")
-	print(str(is_on_floor()) + "Is on Floor")
+		look_at(player.global_transform.origin, Vector3.UP)	
+
+	var current_agent_position : Vector3 = global_transform.origin
+	var next_path_position : Vector3 = nav_agent.get_next_path_position()
+
+	var new_velocity : Vector3 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * run_speed
+#	new_velocity.y = velocity.y
+
+	set_velocity(new_velocity)
 	move_and_slide()
 
 
@@ -70,8 +89,17 @@ func _on_hit(damage = 0.0, pen_rating = 0, col:KinematicCollision3D = null) -> f
 func _on_detect_radius_body_entered(body):
 	if body is Player:
 		player = body
+		set_movement_target(player.global_transform.origin)
+		repath_timer.start()
 
 
 func _on_detect_radius_body_exited(body):
 	if body is Player:
 		player = null
+		set_movement_target(self.global_transform.origin)
+		repath_timer.stop()
+
+
+func _on_repath_timer_timeout():
+	if player:
+		set_movement_target(player.global_transform.origin)
