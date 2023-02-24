04a080febbd5a997ce86481c01a7e09ee761f72c
MenacingMachina <menacingmachina@gmail.com>
2023-02-24 15:57:41 -0500

    Added combat Roomba Mesh

--- Scenes/Player/HitBox.gd
+++ Scenes/Player/HitBox.gd
@@ -5,7 +5,7 @@ signal took_damage(damage:float)
 @export var damage_multiplier = 1.0
 
 func _on_hit(damage = 0.0, pen_rating = 0, col:KinematicCollision3D = null) -> float:
-	damage = damage * damage_multiplier
+damage = damage * damage_multiplier
 	print("Took %s damage, pen rating %s at %s" % [damage, pen_rating, col.get_position()])
 	emit_signal("took_damage", damage)
 	return pen_ratio
