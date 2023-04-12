e892c53f1cfe95aad6e2bafbc94f5f874156167f
MenacingMachina <menacingmachina@gmail.com>
2023-04-12 13:42:57 -0400

    Added Loot area and container population system

---
+++ Scenes/items/LootInformation.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name LootInformation
+
+@export var name:String
+@export var spawn_weight:int
+@export var scene:PackedScene
+@export var test_radius:float
+@export var min_stack:int = 0
+@export var max_stack:int = 0
