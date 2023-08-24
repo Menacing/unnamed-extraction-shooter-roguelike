56c8e67ba972450de38ce8147fe4ad8d97abde22
Menacing <MenacingMachina@gmail.com>
2023-08-24 15:20:21 -0400

    fixed mouse pause menu bug

--- ui/menu/pause.gd
+++ ui/menu/pause.gd
@@ -4,6 +4,8 @@ extends ColorRect
 @onready var quit_button:Button = find_child("quit_game_button")
 @onready var options_button:Button = find_child("options_button")
 
+var original_mouse_mode:Input.MouseMode
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	resume_button.pressed.connect(unpause)
@@ -21,11 +23,12 @@ func _process(delta):
 
 func unpause():
 	get_tree().paused = false
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = original_mouse_mode
 	MenuManager.load_menu(MenuManager.MENU_LEVEL.NONE)
 	
 func pause():
 	get_tree().paused = true
+	original_mouse_mode = Input.mouse_mode
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
 func _on_options_pressed():
