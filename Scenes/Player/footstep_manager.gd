4614280720e07f752d2dae8980338bfc3b294abd
Menacing <MenacingMachina@gmail.com>
2023-11-16 21:45:43 -0500

    Added sound effects and beginning of footstep system

---
+++ Scenes/Player/footstep_manager.gd
@@ -0,0 +1,11 @@
+extends Node3D
+class_name footstep_manager
+
+@onready var audio_player = $FootstepAudioStreamPlayer3D
+@onready var timer = $FoostepTimer
+
+
+
+func _physics_process(delta):
+	pass
+
