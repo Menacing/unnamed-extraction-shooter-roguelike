ad58c9fca9941780376eb8c882c3b73d76342114
Menacing <MenacingMachina@gmail.com>
2023-12-05 16:52:52 -0500

    added basic main menu scene

---
+++ ui/menu/main_menu.gd
@@ -0,0 +1,23 @@
+extends ColorRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_demo_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_game_button_pressed():
+	get_tree().quit
