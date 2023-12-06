dd7c3af734288b41a900839e0da8cb1a206fd48f
Menacing <MenacingMachina@gmail.com>
2023-12-05 21:42:46 -0500

    172 add credits screen (#182)
    * first pass of the credits screen.
    
    Still need to add more credits
    
    * removed unused assets. Added more credits
    
    * swapped icon packs

---
+++ ui/menu/credits/credits.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+func _ready():
+	%godot_logo.tooltip_text = Engine.get_license_text()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		go_back()
+
+func _on_back_button_pressed():
+	go_back()
+
+func go_back():
+	MenuManager.load_menu(MenuManager.previous_menu_level)
+
