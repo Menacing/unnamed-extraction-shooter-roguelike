bd25d03f3c78b64d8d1cdf620617462ff7218d05
Menacing <MenacingMachina@gmail.com>
2023-12-05 19:24:45 -0500

    first pass of the credits screen.
    Still need to add more credits

--- game_logic/menu_manager.gd
+++ game_logic/menu_manager.gd
@@ -4,13 +4,15 @@ enum MENU_LEVEL {
 	NONE,
 	MAIN,
 	PAUSE,
-	OPTIONS
+	OPTIONS,
+	CREDITS
 }
 
 var menus = {
 	MENU_LEVEL.MAIN : preload("res://ui/menu/main_menu.tscn").instantiate(),
 	MENU_LEVEL.PAUSE : preload("res://ui/menu/pause.tscn").instantiate(),
-	MENU_LEVEL.OPTIONS: preload("res://ui/menu/options_menu.tscn").instantiate()
+	MENU_LEVEL.OPTIONS: preload("res://ui/menu/options_menu.tscn").instantiate(),
+	MENU_LEVEL.CREDITS: preload("res://ui/menu/credits/credits.tscn").instantiate()
 }
 
 var current_menu:Node = null
