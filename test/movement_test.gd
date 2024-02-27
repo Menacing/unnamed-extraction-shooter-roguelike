3c5eb68a89de72a36c3d85f9a9e9c02ed9658fad
Menacing <MenacingMachina@gmail.com>
2024-02-26 20:00:12 -0500

    Fixed labels

--- test/movement_test.gd
+++ test/movement_test.gd
@@ -17,11 +17,11 @@ func _physics_process(delta: float) -> void:
 	#cb3d.velocity = Vector3(0,0,-test_speed)
 	step_shape.move_step_and_slide(delta)
 	$StaticBody3D/IsOnFloorLabel.text = "Is On Floor: " + str(step_shape.is_on_floor())
-	$StaticBody3D/IsOnWallLabel.text = "Is On Wall: " + str(step_shape.is_on_floor())
+	$StaticBody3D/IsOnWallLabel.text = "Is On Wall: " + str(step_shape.is_on_wall())
 	
 	cb3d.move_and_slide()
 	$CharacterBody3D/IsOnFloorLabel.text = "Is On Floor: " + str(cb3d.is_on_floor())
-	$CharacterBody3D/IsOnWallLabel.text = "Is On Wall: " + str(cb3d.is_on_floor())
+	$CharacterBody3D/IsOnWallLabel.text = "Is On Wall: " + str(cb3d.is_on_wall())
 	
 	
 	pass
