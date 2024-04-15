7fbbd52822349a179fdb915cbf316d76857fbde1
James Murphy <james.murphy@greenstonefcs.com>
2024-04-15 16:30:39 -0400

    Need to fix event propagation

--- test/ui/misc/ammo_subtype_selector_test.gd
+++ test/ui/misc/ammo_subtype_selector_test.gd
@@ -5,10 +5,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	ammo_subtype_selector.start_selection(gun.get_ammo_type(), gun.current_ammo_subtype, gun.get_unselected_ammo_subtypes())
-	
 	pass # Replace with function body.
 
 
+func _gui_input(event):
+	if event.is_action_pressed("change_ammo_subtype"):
+		ammo_subtype_selector.start_selection(gun.get_ammo_type(), gun.current_ammo_subtype, gun.get_unselected_ammo_subtypes())
 
--- ui/misc/ammo_subtype_selector.gd
+++ ui/misc/ammo_subtype_selector.gd
@@ -5,19 +5,27 @@ var subtype_button_scene:PackedScene = preload("res://ui/misc/sub_type_list_item
 
 @onready var ammo_menu = $MarginContainer/VBoxContainer
 
+var _selecting:bool = false
+
 func start_selection(ammo_type:AmmoType, current_subtype:AmmoSubtype, other_subtypes:Array[AmmoSubtype]):
-	self.visible = true
-	
-	var center_button:AmmoSubTypeListItem = subtype_button_scene.instantiate()
-	center_button._ammo_type = ammo_type
-	center_button._ammo_subtype = current_subtype
-	ammo_menu.add_child(center_button)
-	
-	for ost:AmmoSubtype in other_subtypes:
-		var button:AmmoSubTypeListItem = subtype_button_scene.instantiate()
-		button._ammo_type = ammo_type
-		button._ammo_subtype = ost
-		ammo_menu.add_child(button)
+	if !_selecting:
+		_selecting = true
+		self.visible = true
 		
-	center_button.select()
+		var center_button:AmmoSubTypeListItem = subtype_button_scene.instantiate()
+		center_button._ammo_type = ammo_type
+		center_button._ammo_subtype = current_subtype
+		ammo_menu.add_child(center_button)
+		
+		for ost:AmmoSubtype in other_subtypes:
+			var button:AmmoSubTypeListItem = subtype_button_scene.instantiate()
+			button._ammo_type = ammo_type
+			button._ammo_subtype = ost
+			ammo_menu.add_child(button)
+			
+		center_button.select()
 	
+func _gui_input(event):
+	if _selecting and event.is_action_pressed("change_ammo_subtype"):
+		accept_event()
+		
