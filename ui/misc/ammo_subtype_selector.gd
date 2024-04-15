60cff7911d8784131c5340e13086a9ed63acf1e5
James Murphy <james.murphy@greenstonefcs.com>
2024-04-15 13:55:46 -0400

    Removed unneeded addons. Updated gdunit4

--- ui/misc/ammo_subtype_selector.gd
+++ ui/misc/ammo_subtype_selector.gd
@@ -3,7 +3,7 @@ class_name AmmoSubtypeSelector
 
 var subtype_button_scene:PackedScene = preload("res://ui/misc/sub_type_button.tscn")
 
-@onready var radial_menu = $RadialMenu
+@onready var ammo_menu = $VBoxContainer
 
 func start_selection(ammo_type:AmmoType, current_subtype:AmmoSubtype, other_subtypes:Array[AmmoSubtype]):
 	self.visible = true
@@ -11,11 +11,11 @@ func start_selection(ammo_type:AmmoType, current_subtype:AmmoSubtype, other_subt
 	var center_button:AmmoSubTypeButton = subtype_button_scene.instantiate()
 	center_button._ammo_type = ammo_type
 	center_button._ammo_subtype = current_subtype
-	radial_menu.add_child(center_button)
-	radial_menu.center_node = center_button
+	ammo_menu.add_child(center_button)
+	ammo_menu.center_node = center_button
 	
 	for ost:AmmoSubtype in other_subtypes:
 		var button:AmmoSubTypeButton = subtype_button_scene.instantiate()
 		button._ammo_type = ammo_type
 		button._ammo_subtype = ost
-		radial_menu.add_child(center_button)
+		ammo_menu.add_child(center_button)
