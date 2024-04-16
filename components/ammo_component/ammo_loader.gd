d07b7fda8f122ab451eda09a2f33774d0db248f2
James Murphy <james.murphy@greenstonefcs.com>
2024-04-16 09:10:36 -0400

    Fixed reserve label update. Added bullet scene change

--- components/ammo_component/ammo_loader.gd
+++ components/ammo_component/ammo_loader.gd
@@ -2,9 +2,12 @@ extends Node
 
 var _ammo_types:Array[AmmoType]
 var _path = "res://components/ammo_component/ammo_types/"
+var _ammo_type_map:Dictionary = {}
+var _ammo_subtype_map:Dictionary = {}
 
 func _ready():
 	load_ammo_info_from_path(_path)
+	_build_ammo_map()
 
 func load_ammo_info_from_path(path:String):
 	var dir = DirAccess.open(path)
@@ -23,5 +26,18 @@ func load_ammo_info_from_path(path:String):
 	else:
 		print("An error occurred when trying to access the path.")
 
+func _build_ammo_map():
+	for at in _ammo_types:
+		_ammo_type_map[at.name] = at
+		_ammo_subtype_map[at.name] = {}
+		for st in at.sub_types:
+			_ammo_subtype_map[at.name][st.name] = st
+
 func get_ammo_types() -> Array[AmmoType]:
 	return _ammo_types
+
+func get_ammo_subtype(ammo_type:String, ammo_subtype:String) -> AmmoSubtype:
+	return _ammo_subtype_map[ammo_type][ammo_subtype]
+
+func get_ammo_type(ammo_type:String) -> AmmoType:
+	return _ammo_type_map[ammo_type]
