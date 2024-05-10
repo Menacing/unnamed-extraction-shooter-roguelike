2c84075b1928761c6803c1c9e151df316bd26531
Menacing <MenacingMachina@gmail.com>
2024-05-10 16:00:44 -0400

    Added ammo type display mechanism

--- ui/inventory/ammo_type_vbox_container.gd
+++ ui/inventory/ammo_type_vbox_container.gd
@@ -6,6 +6,7 @@ class_name AmmoTypeVBoxContainer
 var subtype_scene:PackedScene = load("res://ui/inventory/ammo_subtype_h_box.tscn")
 
 @onready var ammo_type_name_label:Label = $AmmoTypeHBox/AmmoTypeLabel
+@onready var ammo_type_texture_rect:TextureRect = $AmmoTypeHBox/AmmoTypeDisplayIcon
 
 @export var _actor_node:Node3D
 var _actor_id:int
@@ -15,6 +16,8 @@ func _ready() -> void:
 	if _actor_node:
 		_actor_id = _actor_node.get_instance_id()
 	ammo_type_name_label.text = ammo_type.name
+	if ammo_type.display_icon:
+		ammo_type_texture_rect.texture = ammo_type.display_icon
 	for subtype:AmmoSubtype in ammo_type.sub_types:
 		var subtype_control:AmmoSubtypeHBoxControl = subtype_scene.instantiate()
 		subtype_control._ammo_type = ammo_type
