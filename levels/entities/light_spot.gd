55482b2567a2cfcc0d67e1848974b2053b96209b
Menacing <MenacingMachina@gmail.com>
2024-06-17 16:14:52 -0400

    blocking out more of the dorm area

--- levels/entities/light_spot.gd
+++ levels/entities/light_spot.gd
@@ -4,5 +4,5 @@ extends SpotLight3D
 
 func _func_godot_apply_properties(props: Dictionary) -> void:
 	LightBase._func_godot_apply_properties(self, props)
-	spot_angle = props["angle"] as float
+	spot_angle = props["spot_angle"] as float
 	spot_range = Helpers.convert_quake_unit_to_godot_unit((props["range"] as float))
