ee46dd687e2e492d221e9b2cba1b047838dad007
Menacing <MenacingMachina@gmail.com>
2024-06-25 21:47:46 -0400

    Fixed footstep component

--- components/footstep_component/footstep_component.gd
+++ components/footstep_component/footstep_component.gd
@@ -1,21 +1,41 @@
 extends Node3D
-class_name footstep_manager
+class_name FootstepComponent
 
 @onready var audio_player = $FootstepAudioStreamPlayer3D
 @onready var timer = $FoostepTimer
+@onready var ray_cast:RayCast3D = $RayCast3D
 
 @export var step_delay:float = .2
+@export var default_move_speed = 1.0
 
-func _physics_process(delta):
-	#Check if moving
-	#TODO Fix this
-	#If Moving check if timer is expired
-	if timer.time_left <= 0:
-		#If timer is expired, play step and restart timer
-		audio_player.pitch_scale = randf_range(.8,1.2)
-		audio_player.play()
-		timer.start(step_delay)
+@export var velocity_haver:Node3D
+
+func _ready():
+	var rids_to_exclude:Array[RID] = Helpers.get_all_collision_object_3d_recursive(velocity_haver)
+	for rid:RID in rids_to_exclude:
+		ray_cast.add_exception_rid(rid)
+
+func _on_foostep_timer_timeout():
+		#If moving
+		if velocity_haver and velocity_haver.velocity.length() > 0:
+			#If colliding
+			if ray_cast.is_colliding():
+				var gbs_collider := ray_cast.get_collider() as GeoBallisticSolid
+				#get sound from material
+				if gbs_collider and gbs_collider._footstep_sound:
+					#set sound to player
+					audio_player.stream = gbs_collider._footstep_sound
+					#Set pitch scale
+					audio_player.pitch_scale = randf_range(.9,1.1)
+					#play
+					audio_player.play()
+					#calculate new delay
+					var new_delay:float = step_delay
+					var ratio = velocity_haver.velocity.length() / default_move_speed
+					if ratio > 0:
+						new_delay = new_delay / ratio
+					#restart timer
+					timer.start(new_delay)
 		
-		#TODO write helper class to get sound for stepped on material
-		#TODO scale the delay based on the move speed
+	
 
