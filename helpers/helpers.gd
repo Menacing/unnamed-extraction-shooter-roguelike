cb563f89c3968ca6dde8653a7a23119acee936f4
Ryan Paul <ryan.quincy.paul@gmail.com>
2024-07-03 15:43:07 -0400

    pushing up so i can load on hopefully less cursed pc

--- helpers/helpers.gd
+++ helpers/helpers.gd
@@ -181,3 +181,15 @@ func convert_godot_vector_to_quake_vector(gv:Vector3) -> Vector3:
 	qv.y = convert_quake_unit_to_godot_unit(gv.y)
 	qv.z = convert_quake_unit_to_godot_unit(gv.z)
 	return qv
+
+func get_normalized_random_stack_count(mean: int, max_value: int, random_number_generator: RandomNumberGenerator) -> int:
+	var sigma = (max_value - mean) / 2.0
+	var random_normal_value = random_number_generator.randfn(mean, sigma)
+		
+	if random_normal_value < 1:
+		return 1
+	elif random_normal_value > max_value:
+		return max_value
+	else:
+		return int(snapped(random_normal_value, 1))
+
