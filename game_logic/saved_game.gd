972ed6fe3c1160a66d695871f48a207f4bfc491c
Menacing <MenacingMachina@gmail.com>
2024-07-04 21:07:39 -0400

    Added save game resource start

---
+++ game_logic/saved_game.gd
@@ -0,0 +1,17 @@
+extends Resource
+class_name SavedGame
+
+@export var level_scene_path:String
+
+#things to save
+
+#player information
+#player transform
+#player health
+#player inventory
+
+#all inventories
+
+#enemies
+
+#world items
