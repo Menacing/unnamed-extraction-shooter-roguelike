ceda0c522ff3742b6ac56dd16e244345b16e7436
Menacing <menacingmachina@gmail.com>
2024-08-03 22:31:42 -0400

    working on juice

--- ui/hideout/level_card.gd
+++ ui/hideout/level_card.gd
@@ -19,3 +19,18 @@ func _ready():
 		pass # Replace with function body.
 
 
+
+var tween_hover:Tween
+func _on_mouse_entered():
+	if tween_hover and tween_hover.is_running():
+		tween_hover.kill()
+	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween_hover.tween_property(self, "scale", Vector2(1.2, 1.2), 0.5)
+
+
+func _on_mouse_exited():
+	#reset scale
+	if tween_hover and tween_hover.is_running():
+		tween_hover.kill()
+	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween_hover.tween_property(self, "scale", Vector2.ONE, 0.55)
