1f5b4136f754992a66745546e2950945b55da278
Menacing <MenacingMachina@gmail.com>
2025-03-09 22:32:55 -0400

    Fixed menu checkboxes

--- ui/menu/options_menu.gd
+++ ui/menu/options_menu.gd
@@ -7,17 +7,18 @@ func _unhandled_input(event):
 		go_back()
 
 func _on_back_button_mouse_entered():
-	GGS.play_sfx(GGS.SFX.MOUSE_OVER)
+	GGS.Audio.MouseEntered.play()
+
 
 func _on_back_button_pressed():
-	GGS.play_sfx(GGS.SFX.INTERACT)
+	GGS.Audio.Interact.play()
 	go_back()
 
 func go_back():
 	MenuManager.load_menu(MenuManager.previous_menu_level)
 
 func _on_option_tab_container_tab_hovered(tab):
-	GGS.play_sfx(GGS.SFX.MOUSE_OVER)
+	GGS.Audio.MouseEntered.play()
 
 func _on_option_tab_container_tab_clicked(tab):
-	GGS.play_sfx(GGS.SFX.INTERACT)
+	GGS.Audio.Interact.play()
