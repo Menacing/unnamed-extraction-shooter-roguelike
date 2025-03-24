34908a38fa9e341cd82bf96b99f227a88afe313f
Menacing <MenacingMachina@gmail.com>
2025-03-24 12:03:47 -0400

    Got fade in animation to work!

--- levels/entities/stash_container.gd
+++ levels/entities/stash_container.gd
@@ -9,12 +9,13 @@ enum StashSize {
 }
 
 @onready var item_highlight_m:ShaderMaterial = load("res://themes/item_highlighter_m.tres")
+@onready var dissolve_shader_m:ShaderMaterial = load("res://game_objects/effects/dissolve_shader_material.tres")
 
-@onready var small_stash_2: Node3D = $small_stash2
+@onready var small_stash_2: MeshInstance3D = $small_stash2/small_stash
 @onready var small_stash_collision_shape_3d: CollisionShape3D = $SmallStashCollisionShape3D
 
 
-@onready var medium_stash_2: Node3D = $medium_stash2
+@onready var medium_stash_2: MeshInstance3D = $medium_stash2/medium_stash
 @onready var medium_stash_collision_shape_3d: CollisionShape3D = $MediumStashCollisionShape3D
 
 
@@ -36,19 +37,38 @@ func _ready():
 func _on_stash_size_changed(inv_data:InventoryData, new_size:int) -> void:
 	match new_size:
 		StashSize.SMALL:
-			small_stash_2.visible = true
 			small_stash_collision_shape_3d.disabled = false
-			
-			medium_stash_2.visible = false
 			medium_stash_collision_shape_3d.disabled = true
-		StashSize.MEDIUM:
-			small_stash_2.visible = false
-			small_stash_collision_shape_3d.disabled = true
 			
-			medium_stash_2.visible = true
+			show_mesh_hide_others(small_stash_2, [medium_stash_2])
+		StashSize.MEDIUM:
+			small_stash_collision_shape_3d.disabled = true
 			medium_stash_collision_shape_3d.disabled = false
+			
+			show_mesh_hide_others(medium_stash_2, [small_stash_2])
 		StashSize.LARGE:
 			pass
 		_:
 			printerr("Setting stash to invalide size")
 	pass
+
+func show_mesh_hide_others(mesh_to_show:MeshInstance3D, meshes_to_hide:Array[MeshInstance3D]):
+	for mesh in meshes_to_hide:
+		mesh.visible = false
+		mesh.material_override = null
+	
+	var mesh_material_texture
+	var mesh_textures = Helpers.get_textures_from_mesh(mesh_to_show)
+	if mesh_textures.size() > 0:
+		mesh_material_texture = mesh_textures[0]
+	var tween = get_tree().create_tween()
+	var animation_length = 2.0
+	dissolve_shader_m.set_shader_parameter("dissolveSlider", 1.5)
+	dissolve_shader_m.set_shader_parameter("baseColorTexture", mesh_material_texture)
+	mesh_to_show.material_override = dissolve_shader_m
+	tween.tween_property(mesh_to_show.material_override, "shader_parameter/dissolveSlider", -1.0, animation_length)
+	tween.tween_callback(show_mesh_cleanup.bind(mesh_to_show))
+	mesh_to_show.visible = true
+
+func show_mesh_cleanup(mesh:MeshInstance3D):
+	mesh.material_override = null
