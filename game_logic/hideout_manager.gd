2f80a99a9d96bfe8570bc9e1e97c9b7131e6e58c
Menacing <MenacingMachina@gmail.com>
2025-03-24 12:04:25 -0400

    270 expanded storage hideout upgrade (#526)
    * Created upgrade control
    
    * Stash upgrade functions. Need model
    
    * Added medium stash model
    
    * stash model changes, need to add effect and probably redo texture
    
    * Got fade in animation to work!

--- game_logic/hideout_manager.gd
+++ game_logic/hideout_manager.gd
@@ -10,9 +10,9 @@ var next_map:LevelInformation
 var selected_run_length:GameplayEnums.GameLength
 var current_map_number:int = 0
 var selected_difficulty:GameplayEnums.GameDifficulty
+
+var current_stash_size:StashContainer.StashSize = StashContainer.StashSize.SMALL
 var inventory_data:InventoryData
-
-
 
 func _ready():
 	crafting_materials_resource_group.load_all_into(_crafting_material_definitions)
@@ -34,6 +34,7 @@ func _on_game_saving(save_file:SaveFile):
 	run_save_data.current_map_number = current_map_number
 	run_save_data.difficulty = selected_difficulty
 	run_save_data.crafting_materials = crafting_materials
+	run_save_data.stash_size = current_stash_size
 	save_file.run_save_data = run_save_data
 	pass
 
@@ -44,6 +45,7 @@ func _on_load_game(save_data:LevelEntitySaveData):
 	current_map_number = run_save_data.current_map_number
 	selected_difficulty = run_save_data.difficulty
 	crafting_materials = run_save_data.crafting_materials
+	current_stash_size = run_save_data.stash_size
 	#hideout_menu.load_run_data(run_save_data)
 	pass
 
@@ -54,11 +56,24 @@ func add_crafting_material(material:SlotData) -> int:
 		for cme:CraftingMaterialEntry in crafting_materials:
 			if cme.material_definition.name == mat_info.crafting_material_definition.name:
 				cme.amount += material.quantity * mat_info.amount_per_stack
+				EventBus.material_changed.emit(cme)
 	else:
 		printerr("NO MAPPING FOR ITEM %s" % material.get_item_type_id())
 	
 	return 0
-	
+
+func remove_crafting_material(material_name:String, amount:int):
+	for cme:CraftingMaterialEntry in crafting_materials:
+		if cme.material_definition.name == material_name:
+			cme.amount -= amount
+			EventBus.material_changed.emit(cme)
+
+func get_crafting_material_amount(material_name:String) -> int:
+	for cme:CraftingMaterialEntry in crafting_materials:
+		if cme.material_definition.name == material_name:
+			return cme.amount
+	return 0
+
 func has_extracted_enough() -> bool:
 	match selected_run_length:
 		GameplayEnums.GameLength.SHORT:
@@ -69,3 +84,17 @@ func has_extracted_enough() -> bool:
 			return current_map_number >= 7
 		_:
 			return false
+
+func _on_stash_upgraded():
+	match current_stash_size:
+		StashContainer.StashSize.SMALL:
+			current_stash_size = StashContainer.StashSize.MEDIUM
+		StashContainer.StashSize.MEDIUM:
+			current_stash_size = StashContainer.StashSize.LARGE
+		StashContainer.StashSize.LARGE:
+			printerr("Can't upgrade Stash any farther!")
+		_:
+			printerr("Setting stash to invalide size")
+	
+	inventory_data.set_inventory_size(current_stash_size)
+	pass
--- levels/entities/stash_container.gd
+++ levels/entities/stash_container.gd
@@ -2,7 +2,22 @@
 class_name StashContainer
 extends GeoBallisticSolid
 
+enum StashSize {
+	SMALL = 4,
+	MEDIUM = 8,
+	LARGE = 16
+}
+
 @onready var item_highlight_m:ShaderMaterial = load("res://themes/item_highlighter_m.tres")
+@onready var dissolve_shader_m:ShaderMaterial = load("res://game_objects/effects/dissolve_shader_material.tres")
+
+@onready var small_stash_2: MeshInstance3D = $small_stash2/small_stash
+@onready var small_stash_collision_shape_3d: CollisionShape3D = $SmallStashCollisionShape3D
+
+
+@onready var medium_stash_2: MeshInstance3D = $medium_stash2/medium_stash
+@onready var medium_stash_collision_shape_3d: CollisionShape3D = $MediumStashCollisionShape3D
+
 
 signal toggle_inventory(external_inventory_owner)
 
@@ -12,52 +27,48 @@ func use(player:Player) -> void:
 	#toggle_inventory.emit(self)
 	player.toggle_stash.emit()
 
-@export var container_size:int
 
 func _ready():
-	inventory_data.set_inventory_size(container_size)
+	inventory_data.inventory_size_changed.connect(_on_stash_size_changed)
+	inventory_data.set_inventory_size(HideoutManager.current_stash_size)
 	HideoutManager.inventory_data = inventory_data
-	#if Engine.is_editor_hint():
-		#return
-	##do ballistic solid stuff
-	#super()
-	##instantiate inventory control
-	#hideout_menu = HideoutManager.hideout_menu
-	#stash_inventory = hideout_menu.stash_inventory_control
-	#self.add_child(stash_inventory)
-	#
-	##setup inventory linkages
-	#stash_inventory.container_size = container_size
-	#EventBus.item_picked_up.connect(_on_item_picked_up)
-	#Helpers.apply_material_overlay_to_children(self, item_highlight_m)
-	#EventBus.game_saving.connect(_on_game_saving)
-#
-#func _on_game_saving(save_file:SaveFile):
-	#var save_data:LevelEntitySaveData = LevelEntitySaveData.new()
-	#save_data.node_path = self.get_path()
-	#save_data.additional_data["inventory_id"] = stash_inventory.inventory_id
-	#save_file.level_entity_save_data.append(save_data)
-	#pass
-#
-#func _on_load_game(save_data:LevelEntitySaveData):
-	#stash_inventory.inventory_id = save_data.additional_data["inventory_id"]
-#
-#func use(player:Player):
-	#if stash_inventory.visible:
-		#player.close_inventory()
-		#EventBus.close_inventory.emit(stash_inventory.inventory_id)
-	#else:
-		#player.open_inventory()
-		#EventBus.open_inventory.emit(stash_inventory.inventory_id)
-#
-#func on_inv_closed(player:Player):
-	#EventBus.close_inventory.emit(stash_inventory.inventory_id)
-#
-#
-#func _on_item_picked_up(result:InventoryInsertResult):
-	#if result.inventory_id == stash_inventory.inventory_id:
-		#var item_instance:ItemInstance = InventoryManager.get_item(result.item_instance_id)
-		#var item_3d:Item3D = ItemAccess.get_item_3d(item_instance.id_3d)
-		#Helpers.force_parent(item_3d,self)
-		#item_3d.picked_up()
-		#item_3d.visible = false
+	
+
+func _on_stash_size_changed(inv_data:InventoryData, new_size:int) -> void:
+	match new_size:
+		StashSize.SMALL:
+			small_stash_collision_shape_3d.disabled = false
+			medium_stash_collision_shape_3d.disabled = true
+			
+			show_mesh_hide_others(small_stash_2, [medium_stash_2])
+		StashSize.MEDIUM:
+			small_stash_collision_shape_3d.disabled = true
+			medium_stash_collision_shape_3d.disabled = false
+			
+			show_mesh_hide_others(medium_stash_2, [small_stash_2])
+		StashSize.LARGE:
+			pass
+		_:
+			printerr("Setting stash to invalide size")
+	pass
+
+func show_mesh_hide_others(mesh_to_show:MeshInstance3D, meshes_to_hide:Array[MeshInstance3D]):
+	for mesh in meshes_to_hide:
+		mesh.visible = false
+		mesh.material_override = null
+	
+	var mesh_material_texture
+	var mesh_textures = Helpers.get_textures_from_mesh(mesh_to_show)
+	if mesh_textures.size() > 0:
+		mesh_material_texture = mesh_textures[0]
+	var tween = get_tree().create_tween()
+	var animation_length = 2.0
+	dissolve_shader_m.set_shader_parameter("dissolveSlider", 1.5)
+	dissolve_shader_m.set_shader_parameter("baseColorTexture", mesh_material_texture)
+	mesh_to_show.material_override = dissolve_shader_m
+	tween.tween_property(mesh_to_show.material_override, "shader_parameter/dissolveSlider", -1.0, animation_length)
+	tween.tween_callback(show_mesh_cleanup.bind(mesh_to_show))
+	mesh_to_show.visible = true
+
+func show_mesh_cleanup(mesh:MeshInstance3D):
+	mesh.material_override = null
