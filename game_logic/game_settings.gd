5f61a363d496a6d460fcd534b4a6a1e857fb8c3d
Menacing <MenacingMachina@gmail.com>
2025-03-29 22:37:57 -0400

    reworked GameSettings singleton to work with GGS (#543)

--- game_logic/game_settings.gd
+++ game_logic/game_settings.gd
@@ -1,5 +1,10 @@
 extends Node
 
+func _ready():
+	GGS.setting_applied.connect(_on_ggs_setting_applied)
+	for ggs_setting:ggsSetting in GGS._get_all_settings():
+		_on_ggs_setting_applied(ggs_setting.key, GGS.get_value(ggs_setting))
+
 var both_eyes_open_ads = true
 var toggle_sprint = false
 var toggle_crouch = false
@@ -18,3 +23,22 @@ enum HEALTH_DISPLAY {
 }
 var selected_health_display:HEALTH_DISPLAY = HEALTH_DISPLAY.BAR
 var default_fov:float = 75.0
+
+func _on_ggs_setting_applied(setting: String, value: Variant):
+	match setting:
+		"both_eyes_open":
+			both_eyes_open_ads = value
+		"toggle_sprint":
+			toggle_sprint = value
+		"toggle_crouch":
+			toggle_crouch = value
+		"toggle_lean":
+			toggle_lean = value
+		"toggle_ads":
+			toggle_ads = value
+		"ads_look_factor":
+			ads_look_factor = value
+		"h_mouse_sens":
+			h_mouse_sens = value
+		"v_mouse_sens":
+			v_mouse_sens
