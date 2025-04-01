c1d39eed8b10501d55c6dd4013080a63dd03ee53
Menacing <MenacingMachina@gmail.com>
2025-04-01 08:23:33 -0400

    Update enemy_spawn_manager.gd
    remapping to an internal dictionary so I can avoid using strings for keys, but get around the object comparison issue

--- game_logic/enemy_spawn_manager.gd
+++ game_logic/enemy_spawn_manager.gd
@@ -2,6 +2,14 @@
 extends Node
 
 const ENEMY_SPAWN_DICTIONARY := preload("res://game_logic/enemy_spawn_dictionary.tres")
+var remapped_enemy_spawn_dictionary:Dictionary = {}
+
+func _ready():
+	for key in ENEMY_SPAWN_DICTIONARY.enemy_spawn_dictionary:
+		remapped_enemy_spawn_dictionary[_map_enemy_spawn_key_to_string(key)] = ENEMY_SPAWN_DICTIONARY.enemy_spawn_dictionary[key]
+	
+func _map_enemy_spawn_key_to_string(esk:EnemySpawnKey) -> String:
+	return str(esk.faction) + "-" + str(esk.tier)
 
 func get_enemy_spawn_mapping(esk:EnemySpawnKey):
-	return ENEMY_SPAWN_DICTIONARY.enemy_spawn_dictionary[esk]
+	return remapped_enemy_spawn_dictionary[_map_enemy_spawn_key_to_string(esk)]
