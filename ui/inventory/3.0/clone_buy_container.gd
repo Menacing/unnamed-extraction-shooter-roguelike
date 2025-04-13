3e95ae52dcc88fab1a24e46b2bda7fd6a1ebc21b
Menacing <MenacingMachina@gmail.com>
2025-04-12 21:06:10 -0400

    fixed ui startup bug

--- ui/inventory/3.0/clone_buy_container.gd
+++ ui/inventory/3.0/clone_buy_container.gd
@@ -7,24 +7,20 @@ const STASH_MATERIAL_ENTRY = preload("res://ui/hideout/stash_material_entry.tscn
 
 signal upgrade_triggered
 
-@export var upgrade_requirement:UpgradeRequirement:
-	set(value):
-		upgrade_requirement = value
-		if value:
-			for child in scroll_container.get_children():
-				child.queue_free()
-			for req in value.required_materials:
-				var mat_entry_control = STASH_MATERIAL_ENTRY.instantiate()
-				mat_entry_control.set_crafting_material(req.material, req.amount)
-				scroll_container.add_child(mat_entry_control)
-				if Engine.is_editor_hint():
-					mat_entry_control.owner = get_tree().edited_scene_root
-			$UpgradeButton.disabled = !is_upgrade_available()
-	get:
-		return upgrade_requirement
+@export var upgrade_requirement:UpgradeRequirement
 
 func _ready() -> void:
 	EventBus.material_changed.connect(_on_material_changed)
+	
+	if upgrade_requirement:
+		for child in scroll_container.get_children():
+			child.queue_free()
+		for req in upgrade_requirement.required_materials:
+			var mat_entry_control = STASH_MATERIAL_ENTRY.instantiate()
+			mat_entry_control.set_crafting_material(req.material, req.amount)
+			scroll_container.add_child(mat_entry_control)
+			if Engine.is_editor_hint():
+				mat_entry_control.owner = get_tree().edited_scene_root
 	_on_material_changed(null)
 	
 func _on_material_changed(crafting_material_entry:CraftingMaterialEntry):
