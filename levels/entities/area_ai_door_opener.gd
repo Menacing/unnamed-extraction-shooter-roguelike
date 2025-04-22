bb6837bf858a877cb5f3e1d954b2a846a57a06ea
Menacing <MenacingMachina@gmail.com>
2025-04-22 14:56:02 -0400

    added trenchbroom entity

--- levels/entities/area_ai_door_opener.gd
+++ levels/entities/area_ai_door_opener.gd
@@ -1,11 +1,23 @@
+@tool
 extends Area3D
 class_name AreaAIDoorOpener
 
 @export var door:GeoBallisticDoor
+@export var _target:String
+
+@export var func_godot_properties: Dictionary
+func _func_godot_apply_properties(entity_properties: Dictionary):
+	if 'target' in func_godot_properties:
+		_target = func_godot_properties.target
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
+	
+	if !Engine.is_editor_hint() and _target:
+		var door_node = get_node("%"+_target)
+		if door_node:
+			door = door_node
 
 func _on_body_entered(body:Node3D) -> void:
 	if body is Enemy and door:
