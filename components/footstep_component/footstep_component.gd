c5112ae177ab4748326268d7a55edaf05d752d36
Menacing <MenacingMachina@gmail.com>
2025-06-19 14:11:41 -0400

    Added AI sound system to gunfire and footsteps

--- components/footstep_component/footstep_component.gd
+++ components/footstep_component/footstep_component.gd
@@ -7,8 +7,11 @@ class_name FootstepComponent
 
 @export var step_delay:float = .2
 @export var default_move_speed = 1.0
-
+@export var default_audible_range = 5.0
+@export var default_footstep_sound = preload("res://game_objects/effects/sounds/steps/footstep_concrete_000.ogg")
 @export var velocity_haver:Node3D
+
+
 
 func _ready():
 	#Setting audio_player to sfx bus
@@ -25,19 +28,22 @@ func _on_foostep_timer_timeout():
 			if ray_cast.is_colliding():
 				var gbs_collider := ray_cast.get_collider() as GeoBallisticSolid
 				#get sound from material
+				var sound_to_play
 				if gbs_collider and gbs_collider._footstep_sound:
-					#set sound to player
-					audio_player.stream = gbs_collider._footstep_sound
-					#Set pitch scale
-					audio_player.pitch_scale = randf_range(.9,1.1)
-					#play
-					audio_player.play()
-					#calculate new delay
-					var new_delay:float = step_delay
-					var ratio = velocity_haver.velocity.length() / default_move_speed
-					if ratio > 0:
-						new_delay = new_delay / ratio
-					#restart timer
-					timer.start(new_delay)
-		
-	
+					sound_to_play = gbs_collider._footstep_sound
+				else:
+					sound_to_play = default_footstep_sound
+				#set sound to player
+				audio_player.stream = sound_to_play
+				#Set pitch scale
+				audio_player.pitch_scale = randf_range(.9,1.1)
+				#play
+				audio_player.play()
+				#calculate new delay
+				var new_delay:float = step_delay
+				var ratio = velocity_haver.velocity.length() / default_move_speed
+				if ratio > 0:
+					new_delay = new_delay / ratio
+				#restart timer
+				timer.start(new_delay)
+				EventBus.sound_emitted.emit(velocity_haver, self.global_position, default_audible_range * ratio)
